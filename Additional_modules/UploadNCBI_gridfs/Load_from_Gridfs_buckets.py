397cef9e063ac4a4b88f26cf8b7c707892a6293f
Unknown <mosterho@ramapo.edu>
2020-02-10 10:24:47 -0500

    Commit Gridfs Buckets

--- Additional_modules/UploadNCBI_gridfs/Load_from_Gridfs_buckets.py
+++ Additional_modules/UploadNCBI_gridfs/Load_from_Gridfs_buckets.py
@@ -10,35 +10,34 @@ db = client.Chromosome
 fs = gridfs.GridFSBucket(db)
 
 ### the following prints an index, not readable data
-
 print('print plain fs: ')
 print(fs)
-
 print('\n\nprint plain fs.find(): ')
 print(fs.find())
 
 print('\n\n\n')
 for dataread in fs.find():
-    print('print dataread in loop: ')
+    print('\n\n ***NEW chromosome - Load byte data into variable for each file (chromosome), print dataread in loop: ')
     dataread_actual = dataread.read()
     #print(dataread)   # prints an index
-    print('The following is the first attempt to make a list of byte data')
-    dataread_string = dataread_actual.split(b'\n')
-    #print(dataread_string)  # prints the nucleic sequence, but includes \n, not a list-- simply a dump
-
-    ### the following properly breaks out the string (in bytes) into lines
-    #print('The following prints the lines as byte data')
-    #for dataread_bytelines in dataread_actual.split(b'\n'):
-    #    print(dataread_bytelines)
+    print('Create a list of byte data from the file/bucket, SPLIT off newline')
+    dataread_actual_bytelist = dataread_actual.split(b'\n')
+    print('use FOR loop to print the first 5 lines of the list')
+    for i in range(5):
+        print(dataread_actual_bytelist[i])
 
     ### now try converting byte data to string using UTF-8 note: this works very well
-    print('The following prints the lines as string date after using ''str''')
-    #for dataread_string in str(dataread_actual, encoding='utf-8').split('\n'):
-    str(dataread_actual, encoding='utf-8').split('\n')
-        #print(dataread_string)
+    print('Use ''str'' to convert byte data to "regular" list and SPLIT off newline')
+    dataread_actual_list = str(dataread_actual, encoding='utf-8').split('\n')
+    print('use FOR loop to print the first 5 lines of the list')
+    for i in range(5):
+        print(dataread_actual_list[i])
+
     # now try removing the first element of the list
-    dataread_string2 = dataread_string.pop(0)
-    print('print dataread_string2 - should be first line of chromosome file')
-    print(dataread_string2)
-    #print('print dataread_string after pop - should be only nucleotides')
-    #print(dataread_string)
+    print('Use POP to remove the first line (this contains the >gi| and description of the file)')
+    dataread_actual_firstline = dataread_actual_list.pop(0)
+    print('print dataread_actual_firstline - should be first line of chromosome file')
+    print(dataread_actual_firstline)
+    print('print dataread_actual_list after pop - should be only nucleotides')
+    for i in range(5):
+        print(dataread_actual_list[i])
